c032a1db56dc309254870bb4b9b659d906e15729
Tyler Ronning <tylerronning03@gmail.com>
2024-11-13 01:19:44 -0800

    Implemented addTemp func which includes collision handeling

--- main.c
+++ main.c
@@ -4,51 +4,115 @@
 #define cityNameLength 101
 #define tempLength 6
 
-int main(int argc, char *argv[]) {
+// know that using mmap will be useful when getting/opening file
+
+int main(int argc, char *argv[])
+{
 
     return 0;
     // open file
-
+    
     // line by line
 
     // add temp
 }
-
+/// @brief stores all information related to the city
 typedef struct City
 {
-    /* data */
-    char* cityName;
-    float sum;
+    char *cityName;
+    double sum;
     float max;
     float min;
     int tempsCount;
 };
 
-
-
-typedef struct hTable {
+/// @brief Table of buckets with length 10000(tableSize)
+typedef struct hTable
+{
     // array of city structs
-
-
+    struct City table[tableSize];
 };
 
-void addTemp (char* cityName, float temp) {
+void addTemp(struct hTable table, char *cityName, float temp)
+{
     // Check hash table for cityName
+    int expectedLoc = hash(cityName);
 
-    // If it doesn't exist:
-        // add to Hash table
-        // make max and min set to this value
-        // set name
-        // set sum to this value
+    // search for city
+    // 1. go to the expected location calculated above
+    if (table.table[expectedLoc].cityName == cityName) // it exists
+    { 
+        
+        table.table[expectedLoc].sum += temp; // add to sum
+        table.table[expectedLoc].tempsCount += 1; // add 1 to count
+    
+        if (table.table[expectedLoc].min > temp) // check against min and max
+        {
+            table.table[expectedLoc].min = temp;
+        }
+        else if (table.table[expectedLoc].max < temp)
+        {
+            table.table[expectedLoc].max = temp;
+        }
 
-    // else it exists:
-        // add to sum
-        // add 1 to count
+        return;
+    }
+    // make new var for loop condition
+    int searchLoc = expectedLoc + 1;
+    while (searchLoc != expectedLoc)
+    {
+        if (table.table[searchLoc].cityName == cityName)
+        {
+            // city found
+            table.table[searchLoc].sum += temp;
+            table.table[searchLoc].tempsCount += 1;
+            if (table.table[searchLoc].min > temp)
+            {
+                table.table[searchLoc].min = temp;
+            }
+            else if (table.table[searchLoc].max < temp)
+            {
+                table.table[searchLoc].max = temp;
+            }
 
-
+            return;
+        }
+        else if (table.table[searchLoc].cityName == NULL) // unsure about value being NULL
+        {
+            // add city at this pos
+            table.table[searchLoc].cityName = cityName;
+            table.table[searchLoc].max = temp;
+            table.table[searchLoc].min = temp;
+            table.table[searchLoc].sum = temp;
+            table.table[searchLoc].tempsCount = 1;
+            // make max and min set to this value
+            // set name
+            // set sum to this value
+            return;
+        }
+        // if the bucket is not the city and not NULL it is filled so go to next bucket due to linear searching
+        // if we reach the end of the table, loop back to the front
+        if (searchLoc == tableSize - 1)
+        {
+            searchLoc = 0;
+        }
+        // otherwise move to next bucket for searching
+        else
+        {
+            searchLoc += 1;
+        }
+    }
 };
-
-int hash (char* city) {
-
+/// @brief turns the name of the city into the index for storing, PsudoCode from https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
+/// @param city the name of the city upon which the hash func will create the index
+/// @return The index in the array where the city should be placed
+int hash(char *city)
+{
+    int hashVal = 2166136261;
+    for (int i = 0; i < strlen(city); i++)
+    {
+        hashVal = hashVal ^ city[i];
+        hashVal = hashVal * 1099511628211;
+    }
+    return;
 }
-// Hash table struct
